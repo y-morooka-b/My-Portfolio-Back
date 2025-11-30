ffa1c63731dbf9480adfb305d588f104888c5b11
y-morooka-b <y.morooka.0318.b@gmail.com>
2025-11-30 19:07:43 +0900

    RequestBase・ResponseBase・ServiseBase の修正・追加

--- household_budget/libraries/core.py
+++ household_budget/libraries/core.py
@@ -1,17 +1,29 @@
-import json
 from django.http import HttpResponse
+from dataclasses import dataclass, asdict
+import json
 import logging
 
 logger = logging.getLogger()
 
+class RequestBase:
+    def __init__(self, **kwargs):
+        pass
+
+    @classmethod
+    def from_json(cls, json_str):
+        data = json.loads(json_str)
+        return cls(**data)
+
+@dataclass
 class ResponseBase:
     def to_json(self):
         """ json文字列にシリアライズ """
-        return json.dumps(self.__dict__, ensure_ascii=False)
+        return json.dumps(asdict(self))
 
-def response(data:ResponseBase):
-    """ レスポンスデータの作成 """
+class ServiseBase:
+    def response(self, data:ResponseBase):
+        """ レスポンスデータの作成 """
 
-    res_json = data.to_json()
-    logger.info(f'<response>: {res_json}')
-    return HttpResponse(res_json)
+        res_json = data.to_json()
+        logger.info(f'<response>: {res_json}')
+        return HttpResponse(res_json)
