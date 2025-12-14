99307895ee554eb7ce86eec07c138c8a427653de
y-morooka-b <y.morooka.0318.b@gmail.com>
2025-12-14 20:50:31 +0900

    viewのファイル分割

--- household_budget/urls.py
+++ household_budget/urls.py
@@ -1,25 +1,18 @@
-# from rest_framework import routers
-# from .views import CategoriesViewSet, test_view_set
-#
-# router = routers.DefaultRouter()
-# router.register('categories', CategoriesViewSet)
-# urlpatterns = router.urls
-
-
 from django.urls import path
-from . import views
+from .views import categories
+from .views import income_and_expenditure
 
 app_name = 'household_budget'
 
 urlpatterns = [
     # カテゴリ関連
-    path('get_categories', views.get_categories, name='get_categories'),
-    path('set_category', views.set_category, name='set_category'),
-    path('del_category', views.del_category, name='del_category'),
-    path('update_category', views.update_category, name='update_category'),
+    path('get_categories', categories.get_categories, name='get_categories'),
+    path('set_category', categories.set_category, name='set_category'),
+    path('del_category', categories.del_category, name='del_category'),
+    path('update_category', categories.update_category, name='update_category'),
     # 収支関連
-    path('get_income_and_expenditure', views.get_income_and_expenditure, name='get_income_and_expenditure'),
-    path('set_income_and_expenditure', views.set_income_and_expenditure, name='set_income_and_expenditure'),
-    path('del_income_and_expenditure', views.del_income_and_expenditure, name='del_income_and_expenditure'),
-    path('update_income_and_expenditure', views.update_income_and_expenditure, name='update_income_and_expenditure'),
+    path('get_income_and_expenditure', income_and_expenditure.get_income_and_expenditure, name='get_income_and_expenditure'),
+    path('set_income_and_expenditure', income_and_expenditure.set_income_and_expenditure, name='set_income_and_expenditure'),
+    path('del_income_and_expenditure', income_and_expenditure.del_income_and_expenditure, name='del_income_and_expenditure'),
+    path('update_income_and_expenditure', income_and_expenditure.update_income_and_expenditure, name='update_income_and_expenditure'),
 ]
--- household_budget/views/income_and_expenditure.py
+++ household_budget/views/income_and_expenditure.py
@@ -1,39 +1,10 @@
-from household_budget.services.del_category_service import RequestDelCategory, DelCategoryService
 from household_budget.services.del_income_and_expenditure import RequestDelIncomeAndExpenditure, DelIncomeAndExpenditureService
-from household_budget.services.get_categories_service import GetCategoriesService
-from household_budget.services.get_income_and_expenditure_record import GetIncomeAndExpenditure, RequestGetIncomeAndExpenditure
-from household_budget.services.set_category_service import SetCategoryService, RequestSetCategory
+from household_budget.services.get_income_and_expenditure_record import RequestGetIncomeAndExpenditure, GetIncomeAndExpenditure
 from household_budget.services.set_income_and_expenditure import RequestSetIncomeAndExpenditure, SetIncomeAndExpenditure
-from household_budget.services.update_category import RequestUpdateCategory, UpdateCategoryService
 from household_budget.services.update_income_and_expenditure import RequestUpdateIncomeAndExpenditure, UpdateIncomeAndExpenditureService
 
-
-# カテゴリ
-def get_categories(request):
-    """ カテゴリ一覧を取得する """
-    obj = GetCategoriesService()
-    return obj.main_process()
-
-def set_category(request):
-    """ カテゴリを設定する """
-    request = RequestSetCategory.from_json(request.body.decode("utf-8"))
-    obj = SetCategoryService(request)
-    return obj.main_process()
-
-def del_category(request):
-    """ カテゴリを削除する """
-    request = RequestDelCategory.from_json(request.body.decode("utf-8"))
-    obj = DelCategoryService(request)
-    return obj.main_process()
-
-def update_category(request):
-    """ カテゴリを更新する """
-    request = RequestUpdateCategory.from_json(request.body.decode("utf-8"))
-    obj = UpdateCategoryService(request)
-    return obj.main_process()
-
-
 # 収支
+
 def get_income_and_expenditure(request):
     """ 収支一覧を取得する """
     request = RequestGetIncomeAndExpenditure.from_json(request.body.decode("utf-8"))
